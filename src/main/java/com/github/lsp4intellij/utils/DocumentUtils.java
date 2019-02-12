5f039aefe598db985a9e434349471c28f115fd58
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2019-02-12 22:16:00 +0530

    Update document utils

--- src/main/java/com/github/lsp4intellij/utils/DocumentUtils.java
+++ src/main/java/com/github/lsp4intellij/utils/DocumentUtils.java
@@ -1,5 +1,6 @@
 package com.github.lsp4intellij.utils;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
@@ -8,10 +9,14 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.DocumentUtil;
 import org.eclipse.lsp4j.Position;
 
+import static com.github.lsp4intellij.utils.ApplicationUtils.computableReadAction;
+
 /**
  * Various methods to convert offsets / logical position / server position
  */
 public class DocumentUtils {
+
+    private static Logger LOG = Logger.getInstance(DocumentUtils.class);
 
     /**
      * Gets the line at the given offset given an editor and bolds the text between the given offsets
@@ -22,17 +27,17 @@ public class DocumentUtils {
      * @return The document line
      */
     public static String getLineText(Editor editor, int startOffset, int endOffset) {
-        // TODO: Fix ApplicationUtil.computableReadAction call
-        Document doc = editor.getDocument();
-        int lineIdx = doc.getLineNumber(startOffset);
-        int lineStartOff = doc.getLineStartOffset(lineIdx);
-        int lineEndOff = doc.getLineEndOffset(lineIdx);
-        String line = doc.getText(new TextRange(lineStartOff, lineEndOff));
-        int startOffsetInLine = startOffset - lineStartOff;
-        int endOffsetInLine = endOffset - lineStartOff;
-        return ApplicationUtils.computableReadAction(
-                () -> line.substring(0, startOffsetInLine) + "<b>" + line.substring(startOffsetInLine, endOffsetInLine)
-                        + "</b>" + line.substring(endOffsetInLine));
+        return computableReadAction(() -> {
+            Document doc = editor.getDocument();
+            int lineIdx = doc.getLineNumber(startOffset);
+            int lineStartOff = doc.getLineStartOffset(lineIdx);
+            int lineEndOff = doc.getLineEndOffset(lineIdx);
+            String line = doc.getText(new TextRange(lineStartOff, lineEndOff));
+            int startOffsetInLine = startOffset - lineStartOff;
+            int endOffsetInLine = endOffset - lineStartOff;
+            return computableReadAction(() -> line.substring(0, startOffsetInLine) + "<b>" + line
+                    .substring(startOffsetInLine, endOffsetInLine) + "</b>" + line.substring(endOffsetInLine));
+        });
     }
 
     /**
@@ -54,13 +59,14 @@ public class DocumentUtils {
      * @return an LSP position
      */
     public static Position offsetToLSPPos(Editor editor, int offset) {
-        // TODO: Fix ApplicationUtil.computableReadAction call
-        Document doc = editor.getDocument();
-        int line = doc.getLineNumber(offset);
-        int lineStart = doc.getLineStartOffset(line);
-        String lineTextBeforeOffset = doc.getText(TextRange.create(lineStart, offset));
-        int column = lineTextBeforeOffset.length();
-        return ApplicationUtils.computableReadAction(() -> new Position(line, column));
+        return computableReadAction(() -> {
+            Document doc = editor.getDocument();
+            int line = doc.getLineNumber(offset);
+            int lineStart = doc.getLineStartOffset(line);
+            String lineTextBeforeOffset = doc.getText(TextRange.create(lineStart, offset));
+            int column = lineTextBeforeOffset.length();
+            return computableReadAction(() -> new Position(line, column));
+        });
     }
 
     /**
@@ -71,23 +77,24 @@ public class DocumentUtils {
      * @return The offset
      */
     public static int LSPPosToOffset(Editor editor, Position pos) {
-        // TODO: Fix ApplicationUtil.computableReadAction call
-        int line = pos.getLine();
-        Document doc = editor.getDocument();
-        String lineText = doc.getText(DocumentUtil.getLineTextRange(doc, line));
-        String lineTextForPosition = lineText.substring(0, Math.min(lineText.length(), pos.getCharacter()));
-        int tabs = StringUtil.countChars(lineTextForPosition, '\t');
-        int tabSize = editor.getSettings().getTabSize(editor.getProject());
-        int column = tabs * tabSize + lineTextForPosition.length() - tabs;
-        int offset = editor.logicalPositionToOffset(new LogicalPosition(line, column));
-        if (pos.getCharacter() >= lineText.length()) {
-            //LOG.warn("LSPPOS outofbounds : " + pos + " line : " + lineText + " column : " + column + " offset : " + offset);
-        }
-        int docLength = doc.getTextLength();
-        if (offset > docLength) {
-            //            LOG.warn("Offset greater than text length : " + offset + " > " + docLength);
-        }
-        return ApplicationUtils.computableReadAction(() -> Math.min(Math.max(offset, 0), docLength));
+        return computableReadAction(() -> {
+            int line = pos.getLine();
+            Document doc = editor.getDocument();
+            String lineText = doc.getText(DocumentUtil.getLineTextRange(doc, line));
+            String lineTextForPosition = lineText.substring(0, Math.min(lineText.length(), pos.getCharacter()));
+            int tabs = StringUtil.countChars(lineTextForPosition, '\t');
+            int tabSize = editor.getSettings().getTabSize(editor.getProject());
+            int column = tabs * tabSize + lineTextForPosition.length() - tabs;
+            int offset = editor.logicalPositionToOffset(new LogicalPosition(line, column));
+            if (pos.getCharacter() >= lineText.length()) {
+                LOG.warn("LSPPOS outofbounds : " + pos + " line : " + lineText + " column : " + column + " offset : "
+                        + offset);
+            }
+            int docLength = doc.getTextLength();
+            if (offset > docLength) {
+                LOG.warn("Offset greater than text length : " + offset + " > " + docLength);
+            }
+            return Math.min(Math.max(offset, 0), docLength);
+        });
     }
-
 }
