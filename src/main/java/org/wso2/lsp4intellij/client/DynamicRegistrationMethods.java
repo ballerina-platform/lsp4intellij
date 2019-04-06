2ffa2c06e584a9cca3a633b1c6e3c393b8aac7ce
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2019-04-06 11:29:19 +0100

    Fix NPE when using dynamic registration options

--- src/main/java/org/wso2/lsp4intellij/client/DynamicRegistrationMethods.java
+++ src/main/java/org/wso2/lsp4intellij/client/DynamicRegistrationMethods.java
@@ -16,22 +16,32 @@
 package org.wso2.lsp4intellij.client;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 /**
  * Enum for methods which may support DynamicRegistration
  */
 public enum DynamicRegistrationMethods {
-    DID_CHANGE_CONFIGURATION("workspace/didChangeConfiguration"), DID_CHANGE_WATCHED_FILES(
-            "workspace/didChangeWatchedFiles"), SYMBOL("workspace/symbol"), EXECUTE_COMMAND(
-            "workspace/executeCommand"), SYNCHRONIZATION("textDocument/synchronization"), COMPLETION(
-            "textDocument/completion"), HOVER("textDocument/hover"), SIGNATURE_HELP(
-            "textDocument/signatureHelp"), REFERENCES("textDocument/references"), DOCUMENT_HIGHLIGHT(
-            "textDocument/documentHighlight"), DOCUMENT_SYMBOL("textDocument/documentSymbol"), FORMATTING(
-            "textDocument/formatting"), RANGE_FORMATTING("textDocument/rangeFormatting"), ONTYPE_FORMATTING(
-            "textDocument/onTypeFormatting"), DEFINITION("textDocument/definition"), CODE_ACTION(
-            "textDocument/codeAction"), CODE_LENS("textDocument/codeLens"), DOCUMENT_LINK(
-            "textDocument/documentLink"), RENAME("textDocument/rename");
+
+    DID_CHANGE_CONFIGURATION("workspace/didChangeConfiguration"),
+    DID_CHANGE_WATCHED_FILES("workspace/didChangeWatchedFiles"),
+    SYMBOL("workspace/symbol"),
+    EXECUTE_COMMAND("workspace/executeCommand"),
+    SYNCHRONIZATION("textDocument/synchronization"),
+    COMPLETION("textDocument/completion"),
+    HOVER("textDocument/hover"),
+    SIGNATURE_HELP("textDocument/signatureHelp"),
+    REFERENCES("textDocument/references"),
+    DOCUMENT_HIGHLIGHT("textDocument/documentHighlight"),
+    DOCUMENT_SYMBOL("textDocument/documentSymbol"),
+    FORMATTING("textDocument/formatting"),
+    RANGE_FORMATTING("textDocument/rangeFormatting"),
+    ONTYPE_FORMATTING("textDocument/onTypeFormatting"),
+    DEFINITION("textDocument/definition"),
+    CODE_ACTION("textDocument/codeAction"),
+    CODE_LENS("textDocument/codeLens"),
+    DOCUMENT_LINK("textDocument/documentLink"),
+    RENAME("textDocument/rename");
 
     private final String name;
 
@@ -39,9 +49,8 @@ public enum DynamicRegistrationMethods {
         this.name = name;
     }
 
-    public static DynamicRegistrationMethods forName(final String name) {
-        return Arrays.stream(DynamicRegistrationMethods.values()).filter(n -> n.name.equals(name))
-                .collect(Collectors.toList()).get(0);
+    public static Optional<DynamicRegistrationMethods> forName(final String name) {
+        return Arrays.stream(DynamicRegistrationMethods.values()).filter(n -> n.name.equals(name)).findAny();
     }
 
     public String getName() {
