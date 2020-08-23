abe482a363e43ecaa1b4c1addf4348b8f27bb098
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2020-08-23 18:13:50 +0530

    Update powermock dependencies to support jdk11

--- src/test/java/org/wso2/lsp4intellij/utils/FileUtilsTest.java
+++ src/test/java/org/wso2/lsp4intellij/utils/FileUtilsTest.java
@@ -34,10 +34,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
+@PowerMockIgnore("jdk.internal.reflect.*")
 public class FileUtilsTest {
 
   @PrepareForTest(FileEditorManager.class)
