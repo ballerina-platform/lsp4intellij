8e73fd14ba5a4d29556d14a8b51a98d4a94eac8d
Markus Schmidt <markus262@web.de>
2020-10-06 00:18:32 +0200

    Revert "naming and earlier "fail fast" check whether a start is needed i.e. is the wrapper created newly" bad idea: -> null -> Npe in connect(Editor); This reverts commit bc03f800

--- src/main/java/org/wso2/lsp4intellij/IntellijLanguageClient.java
+++ src/main/java/org/wso2/lsp4intellij/IntellijLanguageClient.java
@@ -95,10 +95,7 @@ public class IntellijLanguageClient implements ApplicationComponent, Disposable 
         String projectStr = FileUtils.projectToUri(project);
         // find serverdefinition keys for this project and try to start a wrapper
         extToServerDefinition.entrySet().stream().filter(e -> e.getKey().getRight().equals(projectStr)).forEach(entry -> {
-            final LanguageServerWrapper wrapper = addLanguageServerWrapper(project, entry.getKey(), entry.getValue());
-            if (wrapper != null) {
-                wrapper.start();
-            }
+            updateLanguageWrapperContainers(project, entry.getKey(), entry.getValue()).start();
         });
 
     }
@@ -246,17 +243,14 @@ public class IntellijLanguageClient implements ApplicationComponent, Disposable 
                 return;
             }
             // Update project mapping for language servers.
-            LanguageServerWrapper wrapper = addLanguageServerWrapper(project, new ImmutablePair<>(ext, projectUri), serverDefinition);
+            LanguageServerWrapper wrapper = updateLanguageWrapperContainers(project, new ImmutablePair<>(ext, projectUri), serverDefinition);
 
             LOG.info("Adding file " + fileName);
             wrapper.connect(editor);
         });
     }
 
-    /**
-     * Returns the newly created LanguageServerWrapper if necessary; otherwise it returns null
-     */
-    private static synchronized LanguageServerWrapper addLanguageServerWrapper(Project project, final Pair<String, String> key, LanguageServerDefinition serverDefinition) {
+    private static synchronized LanguageServerWrapper updateLanguageWrapperContainers(Project project, final Pair<String, String> key, LanguageServerDefinition serverDefinition) {
         String projectUri = FileUtils.projectToUri(project);
         LanguageServerWrapper wrapper = extToLanguageWrapper.get(key);
         String ext = key.getLeft();
@@ -275,11 +269,12 @@ public class IntellijLanguageClient implements ApplicationComponent, Disposable 
             Set<LanguageServerWrapper> wrappers = projectToLanguageWrappers
                     .computeIfAbsent(projectUri, k -> new HashSet<>());
             wrappers.add(wrapper);
-            return wrapper;
+
         } else {
             LOG.info("Wrapper already existing for " + ext + " , " + projectUri);
         }
-        return null;
+
+        return wrapper;
     }
 
     /**
