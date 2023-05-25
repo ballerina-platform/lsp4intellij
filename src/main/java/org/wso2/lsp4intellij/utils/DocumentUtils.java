53faf626803adecedb1b403c4ce0ffde1155d196
cpedlar@synopsys.com <cpedlar@synopsys.com>
2023-05-25 08:18:16 -0600

    Sanitize the line and column values if they have been reported as negative values when converting them to an LSP position.

--- src/main/java/org/wso2/lsp4intellij/utils/DocumentUtils.java
+++ src/main/java/org/wso2/lsp4intellij/utils/DocumentUtils.java
@@ -117,8 +117,30 @@ public class DocumentUtils {
             int tabs = StringUtil.countChars(lineTextBeforeOffset, '\t');
             int tabSize = getTabSize(editor);
             int column = lineTextBeforeOffset.length() - tabs * (tabSize - 1);
+
+            // Language server positions must be greater than or equal to 0 according to the LSP specification
+            // https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#position
+            // It is possible for the column (and perhaps line?) to be negative values here
+            // If the value of either is negative, sanitize it to zero
+            line = sanitizeLspPosition(line);
+            column = sanitizeLspPosition(column);
+
             return new Position(line, column);
         });
+    }
+
+    /**
+     * Returns the value if the value is greater than or equal to 0.
+     * If the value is negative returns 0.
+     * 
+     * @param value
+     * @return
+     */
+    private static int sanitizeLspPosition(int value) {
+        if (value < 0) {
+            return 0;
+        }
+        return value;
     }
 
     /**
